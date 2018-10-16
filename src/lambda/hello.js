1d10b915874a5ba2e605a3b5a5a9fc52da6e38ef
Minion3665 <pixtopia@null.net>
2018-10-16 13:12:53 +0100

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -6,6 +6,6 @@ export function handler(event, context, callback) {
   console.log(event);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!', ...obj })
+    body: JSON.stringify({ msg: messageOfTheDay, ...obj })
   });
 }
