6fc54b394cba770df54f85286bc2e20dbc05a17d
Minion3665 <pixtopia@null.net>
2018-10-16 14:51:48 +0100

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -3,7 +3,7 @@ const obj = {
   foo: 'bar'
 };
 export function handler(event, context, callback) {
-  console.log(global.motd);
+  console.log("motd:"...global.motd);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: global.motd, ...obj })
