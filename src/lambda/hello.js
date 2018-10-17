399541d6d892931c47aaddbd0703b45a484d3741
Minion3665 <pixtopia@null.net>
2018-10-17 08:23:17 +0100

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -2,7 +2,7 @@ const obj = {
   foo: 'bar'
 };
 export function handler(event, context, callback) {
-  console.log("motd:"... build.environment.motd);
+  console.log(build.environment.motd);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: build.environment.motd, ...obj })
