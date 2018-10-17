9877beadb3664b1e85b94a329f89399b03fb6233
Minion3665 <pixtopia@null.net>
2018-10-17 20:40:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,6 +36,9 @@ class App extends Component {
     return {
       <div className="App">
         <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
           <LambdaDemo />
         </header>
       </div>
