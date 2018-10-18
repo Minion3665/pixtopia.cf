907f1816559dfa0602debc9eb5b67f7962f9ceda
Minion3665 <pixtopia@null.net>
2018-10-18 18:21:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Lambda extends Component {
+class ShowMOTD extends Component {
   constructor(props) {
     super(props);
-    this.state = { loading: false, msg: "Loading MOTD..." };
+    this.state = { loading: false, msg: null };
   }
 
   componentDidMount() {
-    this.setState({ loading: true, msg: "Almost There..."});
-    fetch('/.netlify/functions/getMotd')
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }));
+    const { loading, msg } = this.state;
+    if ({msg} == null) {
+      this.setState({ loading: true});
+      fetch('/.netlify/functions/getMotd')
+        .then(response => response.json())
+        .then(json => this.setState({ loading: false, msg: json.msg }));
+    }
   };
 
   render() {
@@ -30,10 +33,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Pixtopia Gaming Site. Down For Maintenance. Please Bear With Us
-          </p>
-          <Lambda />
+          <html>
+            <div class="header" style="text-align:center">
+              <font size="20" color="#aa0000">Welcome To Pixtopia</font><br/>
+              <ShowMOTD />
+            </div>
+            <div class="bottombar">
+              <span class="link activelink">Home</span>
+              <a href="bugreports.html" class="link">Report a bug</a>
+            </div>
+            <div class="header2" style="text-align:center">
+              <font size="20" color="#aa0000">Welcome To Pixtopia</font><br/>
+              <ShowMOTD />
+            </div>
+            <br/>
         </header>
       </div>
     );
