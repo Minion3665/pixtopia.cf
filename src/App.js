e89d3d7383400a36d2d05feecd801dd5a874987d
Minion3665 <pixtopia@null.net>
2018-10-18 18:57:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class ShowMOTD extends Component {
     document.title = "Welcome to Pixtopia - MOTD: "+{msg}.msg;
     return (
       <React.Fragment>
-        <b>Under Development, Motd: {msg}</b>
+        <b>Status: Under Development, Motd: {msg}</b>
       </React.Fragment>
     );
   }
@@ -29,12 +29,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header>
-            <ShowMOTD />
-            <span className="link activelink">Home</span>
-            <a href="bugreports.html" className="link">Report a bug</a>
-            <font size="20" color="#aa0000">Welcome To Pixtopia</font><br/>
-            <br/>
+        <div class="header" style="text-align:center">
+          <font size="20" color="#aa0000">Welcome To Pixtopia</font><br/>
+          <ShowMOTD />
+        </div>
+        <div class="bottombar">
+          <span className="link activelink">Home</span>
+          <a href="bugreports.html" className="link">Report a bug</a>
+        </div>
         </header>
       </div>
     );
