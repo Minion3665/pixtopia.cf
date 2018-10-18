3121af0547e33335ecf239897fdd4bcfc30358c5
Minion3665 <pixtopia@null.net>
2018-10-18 18:26:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,17 +31,17 @@ class ShowMOTD extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-            <div class="header" style="text-align:center">
+      <div>
+        <header>
+            <div className="header" style="text-align:center">
               <font size="20" color="#aa0000">Welcome To Pixtopia</font><br/>
               <ShowMOTD />
             </div>
-            <div class="bottombar">
-              <span class="link activelink">Home</span>
-              <a href="bugreports.html" class="link">Report a bug</a>
+            <div className="bottombar">
+              <span className="link activelink">Home</span>
+              <a href="bugreports.html" className="link">Report a bug</a>
             </div>
-            <div class="header2" style="text-align:center">
+            <div className="header2" style="text-align:center">
               <font size="20" color="#aa0000">Welcome To Pixtopia</font><br/>
             </div>
             <br/>
