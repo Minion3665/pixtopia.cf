72cc33bb2a329703dd226d26a9235ed8392e2041
Minion3665 <pixtopia@null.net>
2018-10-20 13:30:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,70 @@
-import url from 'url';
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Router, Route, Switch } from 'react-router';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class ShowMOTD extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
 
-  componentDidMount() {
-    const { loading, msg } = this.state;
-    if ({msg}.msg === null) {
-      this.setState({ loading: true});
-      fetch('/.netlify/functions/getMotd')
-        .then(response => response.json())
-        .then(json => this.setState({ loading: false, msg: json.msg }));
-    }
-  };
+      <hr />
 
-  render() {
-    const { loading, msg } = this.state;
-    document.title = "Welcome to Pixtopia - MOTD: "+{msg}.msg;
-    return (
-      <React.Fragment>
-        <b>Status: Under Development, Motd: {msg}</b>
-      </React.Fragment>
-    );
-  }
-}
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
 
-class App extends Component {
-  render() {
-    return (
-      {this.props.children}
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
 
 export default App;
