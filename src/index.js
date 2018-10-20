5f6d07f0968223dec697d88ca282c16dda08af07
Minion3665 <pixtopia@null.net>
2018-10-20 12:32:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { browserHistory } from 'react-router';
 
 import routes from './routes';
 
-ReactDOM.render(<routes history={browserHistory}/>, document.getElementById('root'));
+ReactDOM.render(<routes/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
