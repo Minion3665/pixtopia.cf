c305f2e246809415fdfe02536638dd892e7f7874
Minion3665 <pixtopia@null.net>
2018-10-20 18:36:03 +0100

    Update App.js, (didn't push the commit where everything was, reverting) (can we get a RIP in the comments)

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ class App extends Component {
             <ShowMOTD />
           </div>
           <div className="bottombar">
-            <NavLink to="/" activeClassName="activelink">Home</NavLink>
-            <NavLink to="/about" activeClassName="activelink">About Pixtopia</NavLink>
-            <NavLink to="/report" activeClassName="activelink">Bug Reports</NavLink>
+            <NavLink to="/" className="link" activeClassName="activelink">Home</NavLink>
+            <NavLink to="/about" className="link" activeClassName="activelink">About Pixtopia</NavLink>
+            <NavLink to="/report" className="link" activeClassName="activelink">Bug Reports</NavLink>
           </div>
           <Switch>
             <Route exact path="/" component={Home} />
