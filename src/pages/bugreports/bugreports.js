f92d6cbd0a7aeffba3d37e546ae735e65d2aa8e9
Minion3665 <pixtopia@null.net>
2018-10-20 12:05:55 +0100

    404

---
+++ src/pages/bugreports/bugreports.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './bugreports.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        bug reports
+      </div>
+    );
+  }
+}
+
+export default App;
