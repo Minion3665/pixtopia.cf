53708281e8a2c6e48412f4c1bbb5ab10253b9d02
Minion3665 <pixtopia@null.net>
2018-10-20 07:48:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import url from 'url';
 import React, { Component } from 'react';
 import './App.css';
-import './PageContent';
+import './PageContent.js';
 import {
   BrowserRouter as Router,
   Route,
