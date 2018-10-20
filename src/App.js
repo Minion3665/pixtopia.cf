2a1714741b0e8e4bfb6109ae4f0d0ef9f75d3c31
Minion3665 <pixtopia@null.net>
2018-10-20 14:41:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 class App extends Component {
   render () {
