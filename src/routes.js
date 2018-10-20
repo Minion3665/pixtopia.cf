61051461c999cae5e861fe69b32543a58c95363a
Minion3665 <pixtopia@null.net>
2018-10-20 12:31:55 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { BrowserRouter, Route } from 'react-router';
 
 import App from './pages/index/index.js';
 import bugreports from './pages/bugreports/bugreports.js';
 import notFound from './pages/404/404.js';
 
 const Routes = (props) => (
-  <Router {...props}>
-    <Route path="/" component={App}>
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App}/>
       <Route path="/bugreports" component={bugreports}/>
       <Route path="*" component={notFound}/>
-    </Route>
-  </Router>
+    </div>
+  </BrowserRouter>
 );
 export default Routes;
