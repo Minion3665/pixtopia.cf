7d9d87122b4e1f0754e1971c80edb6a11f52e046
Minion3665 <pixtopia@null.net>
2018-10-20 14:00:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -67,4 +67,31 @@ const Topic = ({ match }) => (
   </div>
 );
 
+class ShowMOTD extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, msg: null };
+  }
+ 
+  componentDidMount() {
+    const { loading, msg } = this.state;
+    if ({msg}.msg === null) {
+      this.setState({ loading: true});
+      fetch('/.netlify/functions/getMotd')
+        .then(response => response.json())
+        .then(json => this.setState({ loading: false, msg: json.msg }));
+    }
+  };
+ 
+  render() {
+    const { loading, msg } = this.state;
+    document.title = "Welcome to Pixtopia - MOTD: "+{msg}.msg;
+    return (
+      <React.Fragment>
+        <b>Status: Under Development, Motd: {msg}</b>
+      </React.Fragment>
+    );
+  }
+}
+
 export default App;
