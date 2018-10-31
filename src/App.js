2a0bc83aa3963283c86541b4ab912d6a84cb0942
Minion3665 <pixtopia@null.net>
2018-10-31 12:58:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
     this.setState({ loading: true});
     fetch('/.netlify/functions/getMotd')
       .then(response => response.json())
-      .then(json => this.setState({ msg: json.msg, status: this.state.status })).then(json => document.title="Welcome To Pixtopia! MOTD: "+json.msg);
+      .then(json => this.setState({ msg: json.msg, status: this.state.status }))
+      .then(json => document.title="Welcome To Pixtopia! MOTD: "+json.msg);
   };
 
   reloadPage() {
@@ -41,7 +42,7 @@ class App extends Component {
             <Online><a href="https://pixtopia.cf/report.html" class="link">Bug Reports</a></Online>
             <Offline><NavLink to="/disabled" activeClassName="activelink" className="link">Join Our Discord!</NavLink></Offline>
             <Online><a href="https://discord.gg/JkqhJWK" class="link">Join Our Discord</a></Online>
-            <div className="hidden" onClick={this.reloadPage}>Check For And Install Updates</div>
+            <div className="updateButton" onClick={this.reloadPage}>Check For And Install Updates</div>
           </div>
           <Switch>
             <Route exact strict path="/" component={Home} />
