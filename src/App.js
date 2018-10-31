63da238e32b060412a32e72d39579de3cb5adfac
Minion3665 <pixtopia@null.net>
2018-10-31 15:01:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
             <Offline><NavLink to="/disabled" activeClassName="activelink" className="link">Bug Reports</NavLink></Offline>
             <Online><a href="https://pixtopia.cf/report.html" class="link">Bug Reports</a></Online>
             <Offline><NavLink to="/disabled" activeClassName="activelink" className="link">Join Our Discord!</NavLink></Offline>
-            <Online><a href="https://discord.gg/JkqhJWK" class="link">Join Our Discord</a></Online>
+            <Online><a href="https://discord.gg/JkqhJWK" class="link">Join Our Discord!</a></Online>
             <Online><div className="updateButton" onClick={this.reloadPage}>Check For And Install Updates</div></Online>
           </div>
           <Switch>
