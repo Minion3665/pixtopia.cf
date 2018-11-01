4c13678e8dc921a8f8cb44aa51aa30b800023321
Minion3665 <pixtopia@null.net>
2018-11-01 14:18:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ class App extends Component {
   render () {
     return (
       <React.Fragment>
-      <div class="startinganimation">
-      <div class="text snackDiv">The Kid's Snack Train</div>
-      <div class="text delightDiv">The Delight Bar</div>
-      <div class="text eveningDiv">The Evening Delicacy Box</div>
-      <div class="text supriseDiv">Suprises in every box</div>
-      <div class="title">Treece & Turner</div>
+        <div class="startinganimation">
+          <div class="text snackDiv">The Kid's Snack Train</div>
+          <div class="text delightDiv">The Delight Bar</div>
+          <div class="text eveningDiv">The Evening Delicacy Box</div>
+          <div class="text supriseDiv">Suprises in every box</div>
+          <div class="title">Treece & Turner</div>
+        </div>
       </React.Fragment>
     );
   }
