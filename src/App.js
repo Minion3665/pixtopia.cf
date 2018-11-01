c671dcc5b08dbbcfb07bff50ea04eff7b6d064e7
Minion3665 <pixtopia@null.net>
2018-11-01 14:10:24 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-  }
+  }
+
   render () {
     return (
-      <React.Fragment>      <div class="startinganimation">
+      <React.Fragment>
+      <div class="startinganimation">
       <div class="text snackDiv">The Kid's Snack Train</div>
       <div class="text delightDiv">The Delight Bar</div>
       <div class="text eveningDiv">The Evening Delicacy Box</div>
-      <div class="text supriseDiv">Suprises in every box</div></a>
+      <div class="text supriseDiv">Suprises in every box</div>
       <div class="title">Treece & Turner</div>
       </React.Fragment>
     );
