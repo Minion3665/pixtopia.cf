175ad9be10ed19287a4538742d3816c457c6b6e5
Minion3665 <pixtopia@null.net>
2018-11-07 12:28:26 +0000

    Update Site to include train images

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
     return (
       <React.Fragment>
         <div class="startinganimation">
-          <a class="snackDiv"><span class="text">The Kid's Snack Train</span></a>
+          <a class="snackDiv" href="train.html"><span class="text">The Kid's Snack Train</span></a>
           <a class="delightDiv"><span class="text">The Delight Bar</span></a>
-          <a class="eveningDiv"><span class="text">The Evening Delicacy Box</span></a>
+          <a class="eveningDiv"><span class="text">The Evening Delicacy Pack</span></a>
           <a class="supriseDiv"><span class="text">Suprises in every box</span></a>
           <div class="title">T & T</div>
         </div>
