d2ea7f5c130a0301e0f692d7d7664f9ab45bd3c7
Minion3665 <pixtopia@null.net>
2018-11-07 12:46:57 +0000

    Then add the link you stupid blob! 1  commit late but still good

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
         <div class="startinganimation">
           <a class="snackDiv" href="train.html"><span class="text">The Kid's Snack Train</span></a>
           <a class="delightDiv" href="delight.html"><span class="text">The Delight Bar</span></a>
-          <a class="eveningDiv"><span class="text">The Evening Delicacy Pack</span></a>
-          <a class="supriseDiv"><span class="text">Suprises in every box</span></a>
+          <a class="eveningDiv" href="evening.html"><span class="text">The Evening Delicacy Pack</span></a>
+          <a class="supriseDiv" href="all.html"><span class="text">Suprises in every box</span></a>
           <div class="title">T & T</div>
         </div>
       </React.Fragment>
